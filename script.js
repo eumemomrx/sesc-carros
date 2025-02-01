167bfb9dcb7cdf90b03a4b614a5c7a07833738da
Robson Santos <robsonlsantos16@gmail.com>
2025-02-01 06:54:29 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const XLSX = require("xlsx");
+const path = require("path");
+
+const caminho = path.join(__dirname, "testecarros.xlsx");
+
+function read(file){
+    const arquivo = XLSX.readFile(file);
+    const pagPlanilha = arquivo.SheetNames[0];
+    const planilha = workbook.Sheets[pagPlanilha];
+
+    const retornoJson = XLSX.utils.sheet_to_json(planilha);
+    return retornoJson;
+
+}
+
+const carros = read(caminho);
+console.log(carros);
